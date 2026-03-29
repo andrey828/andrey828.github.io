99b08d40ee6e382d0294b2c223b19b6788fb31a2
xAndrey <andreymanea03@gmail.com>
2026-03-29 23:44:20 +0200

    Update Program.cs

--- service-csharp/Program.cs
+++ service-csharp/Program.cs
@@ -1,10 +1,6 @@
 using System;
-
-namespace PolyglotCSharp {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("[C#] .NET 8.0 Business Logic Service Started.");
-            Console.WriteLine("[C#] Validating Enterprise constraints...");
-        }
+class Program {
+    static void Main() {
+        Console.WriteLine("[C#] Lógica empresarial verificada.");
     }
 }
