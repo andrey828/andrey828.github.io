ba96bf8f5661c0021fd55f5d163cafc5fba9bbcf
xAndrey <andreymanea03@gmail.com>
2026-03-29 23:43:44 +0200

    Update processor.cpp

--- service-native/processor.cpp
+++ service-native/processor.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main() {
-    std::cout << "[C++] Native Processor Running. High precision calculations active." << std::endl;
+    std::cout << "[C++] Módulo de alto rendimiento activo." << std::endl;
     return 0;
 }
