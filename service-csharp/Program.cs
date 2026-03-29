c92ea4931439f01b8d38e77a6a8de9969777d3e1
xAndrey <andreymanea03@gmail.com>
2026-03-29 23:28:58 +0200

    Create Program.cs

---
+++ service-csharp/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PolyglotCSharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("[C#] .NET 8.0 Business Logic Service Started.");
+            Console.WriteLine("[C#] Validating Enterprise constraints...");
+        }
+    }
+}
