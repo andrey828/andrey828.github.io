8f3e9ea768dfdd9f9b01163b68c3b56c20bbc869
xAndrey <andreymanea03@gmail.com>
2026-03-30 16:54:27 +0200

    Update processor.cpp

--- service-native/processor.cpp
+++ service-native/processor.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <thread>
+
+// Simulación de procesamiento paralelo masivo
 int main() {
-    std::cout << "[C++] Proceso nativo de alto rendimiento finalizado." << std::endl;
+    std::cout << "C++ Hyper-Processor Ready. Multi-threading enabled." << std::endl;
     return 0;
 }
